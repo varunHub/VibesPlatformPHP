0cde7e2ef8470076782ed74ccca420de231b69b4
Bala <bthavapalan@gmail.com>
2014-02-22 16:50:26 -0800

    Formatting Image Handler

--- Utility/Model/ImageHandler.php
+++ Utility/Model/ImageHandler.php
@@ -3,31 +3,31 @@
 class ImageHandler
 {
 
-// File and new size
-function GrapImageFilefromWEB($filename,$toFile)
-{
-	$percent = 0.5;
-	$fwidth = 480;
-	
-	list($width, $height) = getimagesize($filename);
-	$newwidth = $fwidth; //$width; //$width * $percent;
-	$newheight = ($height/$width) * $newwidth; // * $percent;
+	// File and new size
+	function GrapImageFilefromWEB($filename,$toFile)
+	{
+		$percent = 0.5;
+		$fwidth = 480;
+		
+		list($width, $height) = getimagesize($filename);
+		$newwidth = $fwidth; //$width; //$width * $percent;
+		$newheight = ($height/$width) * $newwidth; // * $percent;
 
-	// Load
-	$thumb = imagecreatetruecolor($newwidth, $newheight);
-	$source = imagecreatefromjpeg($filename);
+		// Load
+		$thumb = imagecreatetruecolor($newwidth, $newheight);
+		$source = imagecreatefromjpeg($filename);
 
-	// Resize
-	imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+		// Resize
+		imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
 
-	// Output
-	$toFile = trim($toFile);
-	
-	$Suggested = $toFile;
+		// Output
+		$toFile = trim($toFile);
+		
+		$Suggested = $toFile;
 
-	imagejpeg($thumb,$Suggested);
-	return $Suggested;
-}
+		imagejpeg($thumb,$Suggested);
+		return $Suggested;
+	}
 
 
 }
